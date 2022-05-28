1ead73222321ff9f021d497fc253ddaabf429eb7
Casey Escovedo <caseyjescovedo@gmail.com>
2022-05-28 15:59:57 -0700

    finished

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
-  baseURL: 'https://a15a-76-166-188-40.ngrok.io',
+const instance = axios.create({
+  baseURL: 'https://ecbe-76-166-188-40.ngrok.io',
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
